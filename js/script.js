441e5bd916b0769f9e49739bf08ac04a4993e55b
Takuya Okamoto <t.okamoto@o-bisu.com>
2021-06-24 23:18:56 +0900

    20210624

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 $(function() {
   $(".p-menu__button").click(function() {
     $(".c-sidebar").show("slide" , {direction: "right"} , 200);
+    $(".p-fade-layer").css("visibility","visible");
 
     $(function() {
       $(".c-close__button").click(function() {
         $(".c-sidebar").hide("slide" , {direction: "right"} , 200);
+        $(".p-fade-layer").css("visibility","hidden");
       });
     });
   });
